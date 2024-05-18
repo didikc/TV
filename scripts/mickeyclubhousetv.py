21cd4dd7c6b4c9fd062e3190d7e93fafd679d07e
didikc <166754124+didikc@users.noreply.github.com>
2024-05-18 19:10:30 +0700

    Update mickeyclubhousetv.py

--- scripts/mickeyclubhousetv.py
+++ scripts/mickeyclubhousetv.py
@@ -41,7 +41,7 @@ def grab(url):
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000,CODECS="mp4a.40.34"')
 s = requests.Session()
 with open('../mickeyclubhousetv.txt') as f:
     for line in f:
